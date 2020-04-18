0face9e5e3f4ec5fee81dc839e14b12524020103
Tossao <marcinjamroz8@gmail.com>
2020-04-18 14:05:39 +0200

    Workshop_05 - Zad 3.

---
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -0,0 +1,17 @@
+package pl.coderslab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    @RequestMapping("/books")
+    public class BookController {
+
+        @GetMapping("/hello")
+        public String hello(){
+            return "{hello: World}";
+        }
+    }
+
